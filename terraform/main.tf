45893abefe57684e33296c75932f7ef1e421c3f8
Shaun Prince <shaun@techfusion.ca>
2022-08-02 14:36:01 -0700

    terraform: remove cloud-base components

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,6 @@ data "aws_caller_identity" "current" {}
 
 locals{
   project = "tada-agdata-sales"
-  depends_on = [module.vpc]
-  vpc_id = var.create_vpc ? module.vpc.vpc_id : var.vpc_id
-  subnet_ids = var.create_vpc ? module.vpc.private_subnets : var.private_subnets_ids
-  public_subnet_ids = var.create_vpc ? module.vpc.public_subnets : var.public_subnets_ids
-
   tada_releases_s3 = "tada-releases-${var.env}"
   sales_report_lambda_name = "tada-agdata-sales-report"
   sales_report_lambda_s3 = "${local.sales_report_lambda_name}-app-${var.env}"

c8f241108bd1ed485d3119bdc0c10619f853f455
Shaun Prince <shaun@techfusion.ca>
2022-08-02 18:45:04 -0700

    simplify Dockerfile paths

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -11,7 +11,8 @@ resource "null_resource" "image_build" {
     command = <<EOT
     docker login -u AWS -p $(aws ecr get-login-password --region us-east-1 ) ${local.base_ecr_url}
     echo "############ building Image and tagging with sha ############"
-    docker build -t ${local.base_ecr_url}:${local.image_data_version} -f ../src/Dockerfile .
+    cd ../src
+    docker build -t ${local.base_ecr_url}:${local.image_data_version} -f Dockerfile .
     echo "############ Tagging Image ############"
     docker tag ${local.base_ecr_url}:${local.image_data_version} ${local.base_ecr_url}:latest
     docker tag ${local.base_ecr_url}:${local.image_data_version} ${local.base_ecr_url}:${var.env}

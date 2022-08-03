39055e8d4e805d961af3f834807ebd66ad29aefd
Shaun Prince <shaun@techfusion.ca>
2022-08-02 19:25:07 -0700

    Terraform: deploy Lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -25,31 +25,31 @@ resource "aws_iam_policy" "agdata_sales_lambda_iam_policy" {
   }))
 }
 
-#module "agdata_sales_report_lambda" {
-#  depends_on    = [null_resource.image_build, aws_ecr_repository.ecr_cbin]
-#  version       = "1.0.0"
-#  source        = "terraform-aws-modules/lambda/aws"
-#  runtime       = "python3.9"
-#  function_name = "${local.sales_report_lambda_name}-${var.env}"
-#  description   = "AgData Weekly Sales Report"
-#  image_config_command = ["handler.sales_report_handler"]
-#  image_uri            = "${local.base_ecr_url}:${local.image_data_version}"
-#  create_package       = false
-#  package_type         = "Image"
-#  memory_size          = 1024
-#  timeout              = 180
-#  #create_role    = false
-#  #lambda_role    = aws_iam_role.ingestion_lambda_execution_role.arn
-#  policy = aws_iam_policy.agdata_sales_lambda_iam_policy.id
-#
-#  environment_variables = {
-#    RAW_PREFIX    = "${var.raw_data_path}"
-#    OUTPUT_PREFIX = "${var.output_data_path}"
-#    RAW_BUCKET    = "${local.project}-angus"
-#    OUTPUT_BUCKET = "${local.output_bucket}"
-#    STAGE         = "${var.env}"
-#  }
-#  tags = merge(local.common_tags, tomap({
-#    "Name" = "${local.tada_agdata_sales_name}-report"
-#  }))
-#}
+module "agdata_sales_report_lambda" {
+  depends_on    = [null_resource.image_build, aws_ecr_repository.ecr_cbin]
+  version       = "1.0.0"
+  source        = "terraform-aws-modules/lambda/aws"
+  runtime       = "python3.9"
+  function_name = "${local.sales_report_lambda_name}-${var.env}"
+  description   = "AgData Weekly Sales Report"
+  image_config_command = ["handler.sales_report_handler"]
+  image_uri            = "${local.base_ecr_url}:${local.image_data_version}"
+  create_package       = false
+  package_type         = "Image"
+  memory_size          = 1024
+  timeout              = 180
+  #create_role    = false
+  #lambda_role    = aws_iam_role.ingestion_lambda_execution_role.arn
+  policy = aws_iam_policy.agdata_sales_lambda_iam_policy.id
+
+  environment_variables = {
+    RAW_PREFIX    = "${var.raw_data_path}"
+    OUTPUT_PREFIX = "${var.output_data_path}"
+    RAW_BUCKET    = "${local.project}-angus"
+    OUTPUT_BUCKET = "${local.output_bucket}"
+    STAGE         = "${var.env}"
+  }
+  tags = merge(local.common_tags, tomap({
+    "Name" = local.sales_report_lambda_name
+  }))
+}

5da7a7d69c38a141bb9c716666670fa1c93efaf5
Shaun Prince <shaun@techfusion.ca>
2022-08-02 22:17:33 -0700

    Terraform: tighten lakeformation dependencies

--- terraform/lakeformation.tf
+++ terraform/lakeformation.tf
@@ -5,16 +5,16 @@ locals {
   main_admin_role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/${var.region}/AWSReservedSSO_AWSAdministratorAccess_${var.SSOAdminUserId}"
 }
 
+resource "aws_lakeformation_resource" "data_product" {
+  arn = module.tada_agdata_sales_dataset_s3.s3_bucket_arn
+}
+
 resource "aws_lakeformation_resource" "sales_report" {
-  arn = "arn:aws:s3:::${local.grower_extract_lambda_s3}"
+  arn = module.tada_agdata_sales_app_s3.s3_bucket_arn
 }
 
 resource "aws_lakeformation_resource" "grower_extract" {
-  arn = "arn:aws:s3:::${local.sales_report_lambda_s3}"
-}
-
-resource "aws_lakeformation_resource" "data_product" {
-  arn = "arn:aws:s3:::${local.dataset_s3}"
+  arn = module.tada_agdata_grower_extract_app_s3.s3_bucket_arn
 }
 
 resource "aws_lakeformation_data_lake_settings" "data_lake_settings" {
@@ -29,6 +29,15 @@ resource "aws_lakeformation_permissions" "glue_db_permissions" {
   database {
     name       = aws_glue_catalog_database.agdata_glue_database.name
     catalog_id = aws_glue_catalog_database.agdata_glue_database.catalog_id
+  }
+}
+
+resource "aws_lakeformation_permissions" "data_product_s3_permissions" {
+  principal   = aws_iam_role.glue_role.arn
+  permissions = ["DATA_LOCATION_ACCESS"]
+
+  data_location {
+    arn = aws_lakeformation_resource.data_product.arn
   }
 }
 
@@ -47,14 +56,5 @@ resource "aws_lakeformation_permissions" "grower_extract_s3_permissions" {
 
   data_location {
     arn = aws_lakeformation_resource.grower_extract.arn
-  }
-}
-
-resource "aws_lakeformation_permissions" "data_product_s3_permissions" {
-  principal   = aws_iam_role.glue_role.arn
-  permissions = ["DATA_LOCATION_ACCESS"]
-
-  data_location {
-    arn = aws_lakeformation_resource.data_product.arn
   }
 }

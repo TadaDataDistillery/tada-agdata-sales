227eb2a0b86e11f10266de787a351139df620d9c
Shaun Prince <shaun@techfusion.ca>
2022-08-02 17:20:12 -0700

    Terraform: create ECR repository for Lambda images

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,23 @@
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
 locals{
+  # Project settings
   project = "tada-agdata-sales"
-  tada_releases_s3 = "tada-releases-${var.env}"
-  sales_report_lambda_name = "tada-agdata-sales-report"
-  sales_report_lambda_s3 = "${local.sales_report_lambda_name}-app-${var.env}"
-  sales_report_lambda_input_prefix = "/input"
+  image_major_version = "0"
+  image_minor_version = "9"
+  image_patch_version = format("%s-%s-%s", "local", local.image_major_version, local.image_minor_version)
+  ecr_address        = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.current.account_id, data.aws_region.current.name)
+  base_ecr_url       = "${local.ecr_address}/${var.project}_ecr_lambda_${var.env}"
+  image_data_version = "image_${local.image_major_version}.${local.image_minor_version}.${local.image_patch_version}"
+  
+  # Lambda Configuration
+  sales_report_lambda_name = "${local.project}-report"
+  sales_report_lambda_s3   = "${local.sales_report_lambda_name}-app-${var.env}"
+  sales_report_lambda_input_prefix  = "/input"
   sales_report_lambda_output_prefix = "/output"
-  base_ecr_url = ""
-  image_data_version = ""
-
+  
+  # Global tagging defaults
   common_tags = {
     Project     = local.project
     Environment = var.env

aca8c5d1e1e64dbd53fb07f11387303643d58d60
Shaun Prince <shaun@techfusion.ca>
2022-08-02 17:29:51 -0700

    Terraform: create and deploy ECR image from source

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,22 +2,22 @@ resource "aws_ecr_repository" "lambda_ecr" {
   name = "${local.project}-lambda-ecr-${var.env}"
 }
 
-#resource "null_resource" "image_build" {
-#  depends_on = [aws_ecr_repository.lambda_ecr]
-#  triggers = {
-#    image_sha = "image_base_build-${local.image_patch_version}"
-#  }
-#  provisioner "local-exec" {
-#    command = <<EOT
-#    docker login -u AWS -p $(aws ecr get-login-password --region us-east-1 ) ${local.base_ecr_url}
-#    echo "############ building Image and tagging with sha ############"
-#    cd ../src
-#    docker build -t ${local.base_ecr_url}:${local.image_data_version} -f Dockerfile .
-#    echo "############ Tagging Image ############"
-#    docker tag ${local.base_ecr_url}:${local.image_data_version} ${local.base_ecr_url}:latest
-#    docker tag ${local.base_ecr_url}:${local.image_data_version} ${local.base_ecr_url}:${var.env}
-#    echo "############ Pushing Image ############"
-#    docker push ${local.base_ecr_url}:${local.image_data_version}
-#    EOT
-#  }
-#}
+resource "null_resource" "image_build" {
+  depends_on = [aws_ecr_repository.lambda_ecr]
+  triggers = {
+    image_sha = "image_base_build-${local.image_patch_version}"
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+    docker login -u AWS -p $(aws ecr get-login-password --region us-east-1 ) ${local.base_ecr_url}
+    echo "############ building Image and tagging with sha ############"
+    cd ../src
+    docker build -t ${local.base_ecr_url}:${local.image_data_version} -f Dockerfile .
+    echo "############ Tagging Image ############"
+    docker tag ${local.base_ecr_url}:${local.image_data_version} ${local.base_ecr_url}:latest
+    docker tag ${local.base_ecr_url}:${local.image_data_version} ${local.base_ecr_url}:${var.env}
+    echo "############ Pushing Image ############"
+    docker push ${local.base_ecr_url}:${local.image_data_version}
+    EOT
+  }
+}

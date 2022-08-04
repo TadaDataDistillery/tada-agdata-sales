1444936e9638256a6f0b9653df458390c71ad1de
Shaun Prince <shaun@techfusion.ca>
2022-08-03 20:08:38 -0700

    Terraform: Consolidate data providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-data "aws_caller_identity" "current" {}
-data "aws_iam_session_context" "current" {
-  arn = data.aws_caller_identity.current.arn
-}
-data "aws_region" "current" {}
 locals{
   # General settings
   project = "tada-agdata-sales"
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,3 +5,11 @@ terraform {
 provider "aws" {
   region  = var.region
 }
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_session_context" "current" {
+  arn = data.aws_caller_identity.current.arn
+}
+
+data "aws_region" "current" {}
